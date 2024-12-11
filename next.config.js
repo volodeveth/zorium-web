cd831bb40528ceb8587b78053c755e5ad313d3e7
volodeveth <starbowshine@gmail.com>
2024-12-11 02:03:02 +0200

    Fix dependencies and configuration

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      net: false,
-      tls: false,
-      encoding: false,
-      bufferutil: false,
-      'utf-8-validate': false,
-      'pino-pretty': false
-    };
+    config.externals = [...(config.externals || []), 'encoding', 'bufferutil', 'utf-8-validate'];
     return config;
   },
 };
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,19 +1,9 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiConfig, createConfig, configureChains } from 'wagmi';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import { bsc } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
-import { 
-  connectorsForWallets,
-  RainbowKitProvider,
-  darkTheme
-} from '@rainbow-me/rainbowkit';
-import {
-  metaMaskWallet,
-  trustWallet,
-  walletConnectWallet,
-} from '@rainbow-me/rainbowkit/wallets';
 import '@rainbow-me/rainbowkit/styles.css';
 
 const { chains, publicClient } = configureChains(
@@ -23,16 +13,11 @@ const { chains, publicClient } = configureChains(
 
 const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || '';
 
-const connectors = connectorsForWallets([
-  {
-    groupName: 'Recommended',
-    wallets: [
-      metaMaskWallet({ projectId, chains }),
-      trustWallet({ projectId, chains }),
-      walletConnectWallet({ projectId, chains }),
-    ],
-  },
-]);
+const { connectors } = getDefaultWallets({
+  appName: 'Zorium DApp',
+  projectId,
+  chains
+});
 
 const wagmiConfig = createConfig({
   autoConnect: true,
@@ -40,19 +25,12 @@ const wagmiConfig = createConfig({
   publicClient
 });
 
-const queryClient = new QueryClient();
-
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiConfig config={wagmiConfig}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider 
-          chains={chains}
-          theme={darkTheme()}
-        >
-          {children}
-        </RainbowKitProvider>
-      </QueryClientProvider>
+      <RainbowKitProvider chains={chains}>
+        {children}
+      </RainbowKitProvider>
     </WagmiConfig>
   );
 }
